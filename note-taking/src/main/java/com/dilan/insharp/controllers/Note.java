06b1d5a3c5a254126cacb0bf2cc09ad4e0ab8db4
Dilan-Madusanka <dilanmax93@gmail.com>
2018-11-02 18:06:51 +0530

    V2 Enabled CORS
    Add "@CrossOrigin(origins = "http://localhost:3000")" annotation.

--- note-taking/src/main/java/com/dilan/insharp/controllers/Note.java
+++ note-taking/src/main/java/com/dilan/insharp/controllers/Note.java
@@ -2,6 +2,7 @@ package com.dilan.insharp.controllers;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -19,25 +20,29 @@ public class Note {
 	@Autowired
 	private NoteServices noteServices;
 	
+	@CrossOrigin(origins = "http://localhost:3000")
 	@GetMapping("/all")
 	public List<NoteDTO> allNotes() {
 		return noteServices.findAllNotes();
 	}
 	
+	@CrossOrigin(origins = "http://localhost:3000")
 	@PostMapping("/add")
 	public String addNote(@RequestBody NoteDTO noteData) {
 		return noteServices.saveNote(noteData);
 	}
 	
+	@CrossOrigin(origins = "http://localhost:3000")
 	@PutMapping("/update")
 	public String updateNote(@RequestBody NoteDTO newNote) {
 		return noteServices.updateNote(newNote);
 	}
 	
+//	@CrossOrigin(origins = "http://localhost:3000")
 //	@GetMapping("/find/{id}")
 //	public <NoteDTO getNoteById (@PathVariable Integer id) {
 //		return noteServices.findById(id);
 //	}
-//	
+	
 
 }
